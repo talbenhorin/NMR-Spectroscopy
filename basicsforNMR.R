909c1d53e87ff0a4b202ea92decde36e4e82bc54
Tal Ben-Horin PhD <tbenhor@wolftech.ad.ncsu.edu>
2020-10-30 14:16:12 -0400

    update to Rnmr1D

--- basicsforNMR.R
+++ basicsforNMR.R
@@ -1,9 +1,5 @@
 rm(list=ls(all=TRUE))
 
-install.packages("speaq")
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install("MassSpecWavelet")
-BiocManager::install("impute")
+#install.packages("Rnmr1D")
+library(Rnmr1D)
 
-library(speaq)
